944ed7cea12ec88b0542ad0b28059eb078efdeab
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-11-12 23:57:19 -0300

    refactor: move create account route to a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,19 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
-import { z } from "zod";
-import { v4 as uuidv4 } from "uuid";
+import { accountRoutes } from "routes/account";
 
 const port = 3334;
-
-const customers = [];
 
 async function bootastrap() {
   const app = fastify({ logger: true });
 
-  app.register(cors, { origin: true });
+  await app.register(cors, { origin: true });
 
   app.get("/", (request, response) => {
     return response.send("Tudo OK");
   });
 
-  app.post("/account", async (request, response) => {
-    const validationSchema = z.object({
-      cpf: z.string(),
-      name: z.string(),
-    });
-
-    const { cpf, name } = validationSchema.parse(request.body);
-
-    const id = uuidv4();
-
-    const customer = {
-      id,
-      cpf,
-      name,
-      statements: [],
-    };
-
-    customers.push(customer);
-
-    return response.send(customer);
-  });
+  await app.register(accountRoutes);
 
   app.listen({ port }, () => {
     console.log(`Server running at port ${port}`);
---
+++ src/routes/account.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { v4 as uuidv4 } from "uuid";
+
+const customers = [];
+
+export async function accountRoutes(fastify: FastifyInstance) {
+  fastify.post("/account", async (request, response) => {
+    const validationSchema = z.object({
+      cpf: z.string(),
+      name: z.string(),
+    });
+
+    const { cpf, name } = validationSchema.parse(request.body);
+
+    const id = uuidv4();
+
+    const customer = {
+      id,
+      cpf,
+      name,
+      statements: [],
+    };
+
+    customers.push(customer);
+
+    return response.send(customer);
+  });
+}
