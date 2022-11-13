3e51105a49ff56428e2d2390bf488a976c8785b9
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-11-12 23:53:39 -0300

    feat: create account sample route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import { z } from "zod";
+import { v4 as uuidv4 } from "uuid";
 
 const port = 3334;
+
+const customers = [];
 
 async function bootastrap() {
   const app = fastify({ logger: true });
@@ -10,6 +14,28 @@ async function bootastrap() {
 
   app.get("/", (request, response) => {
     return response.send("Tudo OK");
+  });
+
+  app.post("/account", async (request, response) => {
+    const validationSchema = z.object({
+      cpf: z.string(),
+      name: z.string(),
+    });
+
+    const { cpf, name } = validationSchema.parse(request.body);
+
+    const id = uuidv4();
+
+    const customer = {
+      id,
+      cpf,
+      name,
+      statements: [],
+    };
+
+    customers.push(customer);
+
+    return response.send(customer);
   });
 
   app.listen({ port }, () => {
