15c9c779c5c65aae397504d6eca7c589a094017b
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-11-14 20:24:32 -0300

    chore: clean unused imports

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,11 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
-import { v4 as uuidv4 } from "uuid";
-import { CustomerRepository } from "repositories/CustomerRepository";
 import { CustomerController } from "controllers/customer-controller";
 
 export async function accountRoutes(fastify: FastifyInstance) {
-  const customerRepository = new CustomerRepository();
-
   fastify.post("/account", new CustomerController().createCustomer);
 }
